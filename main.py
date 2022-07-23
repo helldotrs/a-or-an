aa0b48b720eb1a8db7d13f1845d0ea6407c2de66
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-07-23 21:51:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 vowels  = ["a", "e", "i", "o", "u"]
 
 while(True):
-    word    = input("type string+[enter], or [ctrl]+[c] to exit:")[0]
+    string    = input("type string+[enter], or [ctrl]+[c] to exit:")[0]
 
-    if word in vowels:
+    if string in vowels:
         print("an")
     else:
         print("a")
