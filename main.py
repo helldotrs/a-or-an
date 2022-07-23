5bb3e4920333e8e798da44d9d7d326a596b55cec
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-07-23 21:52:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ vowels  = ["a", "e", "i", "o", "u"]
 while(True):
     first_char    = input("type string+[enter], or [ctrl]+[c] to exit:")[0]
 
-    if frist_char in vowels:
+    if first_char in vowels:
         print("an")
     else:
         print("a")
